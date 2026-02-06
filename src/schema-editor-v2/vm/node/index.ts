9ae4188b756f878826fae7c85999e0abf5422f29
Anton Kashirov <anton.kashirov@gmail.com>
2026-02-06 09:31:33 +0300

    chore: remove v2 (#25)

--- src/schema-editor-v2/vm/node/index.ts
+++
@@ -1,6 +0,0 @@
-export { BaseNodeVM } from './BaseNodeVM';
-export { ObjectNodeVM } from './ObjectNodeVM';
-export { ArrayNodeVM } from './ArrayNodeVM';
-export { PrimitiveNodeVM } from './PrimitiveNodeVM';
-export { ForeignKeyNodeVM } from './ForeignKeyNodeVM';
-export { RefNodeVM } from './RefNodeVM';
