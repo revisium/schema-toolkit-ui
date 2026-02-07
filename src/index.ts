341f1797fb0df79a07c7a636f9b76c62958ba658
anton62k <anton.kashirov@gmail.com>
2026-02-07 18:17:48 +0300

    chore: deps

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,12 @@ export {
   JsonSchemaTypeName,
   type JsonObjectSchema,
   type JsonPatch,
+  createTableModel,
+  createRowModel,
+  type TableModel,
+  type TableModelOptions,
+  type RowModel,
+  type RowModelOptions,
 } from '@revisium/schema-toolkit';
+
+export { ensureReactivityProvider } from './lib';
